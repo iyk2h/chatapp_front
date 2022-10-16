50e0a0175feda8ca9ab4fa8faa67c1a76ca30e32
youngkyunIm <yee0230@naver.com>
2022-10-16 18:33:41 +0900

    feat : 사용자별 메세지 표시 분리

--- js/chat.js
+++ js/chat.js
@@ -1,48 +1,78 @@
-const eventSource = new EventSource("http://localhost:8080/sender/test1/receiver/sar");
+let username  = prompt("아이디를 입력하세요")
+let roomNum  = prompt("채팅방 번호를 입력하세요")
+
+document.querySelector("#username").innerHTML = username;
+
+const eventSource = new EventSource(`http://localhost:8080/chat/roomNum/${roomNum}`);
 
 eventSource.onmessage = (event) => {
-    console.log(1, event);
     const data = JSON.parse(event.data);
-    console.log(2, data);
-    initMessage(data);
+    if(data.sender === username){
+        initMyMessage(data);
+    } else {
+        initYourMessage(data);
+    }
+
 }
 
-function getSendMsgBox(msg, time) {
+// 파란 박스
+function getSendMsgBox(data) {
+    let md = data.createdAt.substring(5,10)
+    let tm = data.createdAt.substring(11,16)
+    convertTime = tm + " | " + md
+
     return `<div class="sent_msg">
-    <p> ${msg} </p>
-    <span class="time_date"> ${time} </span>
+    <p> ${data.msg} </p>
+    <span class="time_date"> ${convertTime} / ${data.sender} </span>
   </div>`;
 }
 
-function initMessage(data){
+// 회색 박스
+function getReceiveMsgBox(data) {
+    let md = data.createdAt.substring(5,10)
+    let tm = data.createdAt.substring(11,16)
+    convertTime = tm + " | " + md
+
+    return `<div class="received_withd_msg">
+    <p> ${data.msg} </p>
+    <span class="time_date"> ${convertTime} / ${data.sender} </span>
+  </div>`;
+}
+
+
+function initMyMessage(data){
     let chatBox = document.querySelector("#chat-box");
-    let msgInput = document.querySelector("#chat-outgoing-msg");
 
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg";
+    let sendBox = document.createElement("div");
+    sendBox.className = "outgoing_msg";
 
-    chatOutgoingBox.innerHTML = getSendMsgBox(data.msg, data.createdAt);
-    chatBox.append(chatOutgoingBox);
-    msgInput.value = "";
+    sendBox.innerHTML = getSendMsgBox(data);
+    chatBox.append(sendBox);
+
+    document.documentElement.scrollTop = document.body.scrollHeight;
+}
+
+function initYourMessage(data){
+    let chatBox = document.querySelector("#chat-box");
+
+    let receivedBox = document.createElement("div");
+    receivedBox.className = "received_msg";
+
+    receivedBox.innerHTML = getReceiveMsgBox(data);
+    chatBox.append(receivedBox);
+    document.documentElement.scrollTop = document.body.scrollHeight;
 }
 
 async function addMessage(){
-    let chatBox = document.querySelector("#chat-box");
     let msgInput = document.querySelector("#chat-outgoing-msg");
 
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg";
-
-    let date = new Date();
-    let now = date.getHours() + ":" + date.getMinutes()+ " | "+ date.getMonth() + "/" + date.getDate();
-
     let chat = {
-        sender: "test1",
-        receiver: "sar",
+        sender: username,
+        roomNum: roomNum,
         msg: msgInput.value
     };
 
-    let response = await fetch("http://localhost:8080/chat", {
+    fetch("http://localhost:8080/chat", {
         method: "post",
         body: JSON.stringify(chat),
         headers: {
@@ -50,18 +80,10 @@ async function addMessage(){
         }
     })
 
-    console.log(response);
-
-    let parseResponse = await response.json();
-    console.log(parseResponse)
-
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
-    chatBox.append(chatOutgoingBox);
     msgInput.value = "";
 }
 
 document.querySelector("#chat-outgoing-button").addEventListener("click", ()=>{
-    // alert("클릭됨");
     addMessage();
 });
 
