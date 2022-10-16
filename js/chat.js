b68004606495447bcd2b6456268a3235d76f3637
youngkyunIm <yee0230@naver.com>
2022-10-16 17:23:14 +0900

    feat : get history message, add date

--- js/chat.js
+++ js/chat.js
@@ -4,13 +4,26 @@ eventSource.onmessage = (event) => {
     console.log(1, event);
     const data = JSON.parse(event.data);
     console.log(2, data);
+    initMessage(data);
 }
 
-function getSendMsgBox(msg) {
+function getSendMsgBox(msg, time) {
     return `<div class="sent_msg">
     <p> ${msg} </p>
-    <span class="time_date"> 11:18 | Today</span>
+    <span class="time_date"> ${time} </span>
   </div>`;
+}
+
+function initMessage(data){
+    let chatBox = document.querySelector("#chat-box");
+    let msgInput = document.querySelector("#chat-outgoing-msg");
+
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+
+    chatOutgoingBox.innerHTML = getSendMsgBox(data.msg, data.createdAt);
+    chatBox.append(chatOutgoingBox);
+    msgInput.value = "";
 }
 
 function addMessage(){
@@ -20,7 +33,10 @@ function addMessage(){
     let chatOutgoingBox = document.createElement("div");
     chatOutgoingBox.className = "outgoing_msg";
 
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
+    let date = new Date();
+    let now = date.getHours() + ":" + date.getMinutes()+ " | "+ date.getMonth() + "/" + date.getDate();
+
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
 }
