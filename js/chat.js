5af42294a86fe2dc055c73ebe6a1daece451029c
youngkyunIm <yee0230@naver.com>
2022-10-16 17:09:46 +0900

    refact : func_addMessge

--- js/chat.js
+++ js/chat.js
@@ -13,8 +13,7 @@ function getSendMsgBox(msg) {
   </div>`;
 }
 
-document.querySelector("#chat-outgoing-button").addEventListener("click", ()=>{
-    // alert("클릭됨");
+function addMessage(){
     let chatBox = document.querySelector("#chat-box");
     let msgInput = document.querySelector("#chat-outgoing-msg");
 
@@ -24,18 +23,15 @@ document.querySelector("#chat-outgoing-button").addEventListener("click", ()=>{
     chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
+}
+
+document.querySelector("#chat-outgoing-button").addEventListener("click", ()=>{
+    // alert("클릭됨");
+    addMessage();
 });
 
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e)=>{
     if(e.keyCode === 13){
-    let chatBox = document.querySelector("#chat-box");
-    let msgInput = document.querySelector("#chat-outgoing-msg");
-
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg";
-
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
-    chatBox.append(chatOutgoingBox);
-    msgInput.value = "";
+        addMessage();
     }
-})
+});
